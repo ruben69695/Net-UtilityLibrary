b579088c81356eb1f39137afd859d668bd77ddd3
Ruben Arrebola <ruben.arre6@gmail.com>
2017-10-08 17:37:59 +0200

    -Se modifican las propiedades del proyecto para obtener comentarios XML y se cambia la versión. -Se añade la clase IOLibrary.cs, con la función de obtener el tamaño de un directorio. - Se añade la clase de conversiones con una nueva función dentro.

---
+++ UtilityLibraries/ConvertLibrary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UtilityLibraries
+{
+    /// <summary>
+    /// Static class that offers universal functions with conversions for .NET Standard Platform (Continuous developing class)
+    /// </summary>
+    public static class Conversion
+    {
+        /// <summary>
+        /// Function to convert bytes to megabytes
+        /// </summary>
+        /// <param name="bytes">Number of bytes</param>
+        /// <returns>Double in megabytes</returns>
+        public static Double FromBytesToMegaBytes(long bytes)
+        {
+            return System.Convert.ToDouble((bytes / (Math.Pow(1024, 2))));
+        }
+    }
+}
