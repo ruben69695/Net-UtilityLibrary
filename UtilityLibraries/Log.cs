b8518a0ba5a1ab02932aaf7bc43901100f91048d
Ruben Arrebola <ruben.arre6@gmail.com>
2017-10-13 00:10:27 +0200

    Se añade un funcionamiento básico se la clase, permite instanciarla pasandole el nombre del archivo, si existe te añade contenido al final y si no lo crea, tiene una función asíncrona que se encarga de añadir la fecha con la hora actual y el texto pasado por parametro para ñadir al fichero.

---
+++ UtilityLibraries/Log.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace UtilityLibraries
+{
+    class Log
+    {
+        private string filename = "";
+
+        public Log(string xfilename)
+        {
+            this.filename = xfilename;
+        }
+
+        public async void AddLog(string text)
+        {
+            if (!(String.IsNullOrWhiteSpace(text)))
+            {
+                UnicodeEncoding unicodeEncoding = new UnicodeEncoding();
+                byte[] result = unicodeEncoding.GetBytes(String.Format("{0} - ", DateTime.Now, text));
+
+                using (FileStream SourceStream = File.Open(this.filename, FileMode.Append))
+                {
+                    await SourceStream.WriteAsync(result, 0, result.Length);
+                }
+            }
+        }
+
+        public void SetFilename(string xfilename)
+        {
+            this.filename = xfilename;
+        }
+
+    }
+}
